4edb634fecffa4a4d944b79f1b079249c82b1c10
NetCinematics <alpinefalcon@protonmail.com>
2023-05-13 12:43:01 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # node_dashboard_server
 server side DEPLOY to VERCEL for React Dashboard (separate)
+
+### backend SETUP
+> npm init
+
+> npm i express axios dotenv cors
+
+> index.js + e-n-v-s
+
+> npm i nodemon
+    "start": "nodemon index.js",
+
+> npm run start
+
+### TEST:
+//npm run start
+//http://localhost:8008/news
+//data
+
+### DEPLOY
+vercel.json...
+https://jonathans199.medium.com/deploy-node-js-express-api-to-vercel-dbf4461795a5
+- login, connect vercel to github
+- vercel, new project connect to repo
+- setup: other ./ 
+- add E N V V A R S
+> DEPLOY btn
+
+> REMEMBER: dev / prod urls
+- dev:    http://localhost:8008
+- devapi: http://localhost:8008/news
+- prod: 
+
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const axios = require('axios');
+require('dotenv').config();
+const port = 8008;
+//INSTRUCTION:
+//npm run start
+//http://localhost:8008/news
+
+app.get('/', (req, res) => {
+  res.send('Holo, wurldz!');
+});
+
+app.get('/news', async (req, res) => {
+
+    const options = {
+      method: 'GET',
+      url: 'https://crypto-news-live9.p.rapidapi.com/news/CryptoNews',
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAD_KEY,
+        'X-RapidAPI-Host': 'crypto-news-live9.p.rapidapi.com'
+      }
+    };
+    
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 404;
+        res.json({data:'err1'});
+    }
+
+
+//   res.json({data:'msg1'});
+});
+
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
+});
+
+// Export the Express API
+module.exports = app
